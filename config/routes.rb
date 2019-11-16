9802d3873bd5523aed8834df963700a3b186e254
Rachel Lew <rlew07@gmail.com>
2019-11-16 15:09:34 -0700

    Adds functionality to delete task

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   get '/tasks/:id', to: 'tasks#show'
   get '/tasks/:id/edit', to: 'tasks#edit'
   patch '/tasks/:id', to: 'tasks#update'
+  delete '/tasks/:id', to: 'tasks#destroy'
 end
